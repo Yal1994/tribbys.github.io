87b2f177cc69b09b653c345c82081ef95774cf0f
yal <yanl@fiberhome.com>
2020-06-26 23:42:49 +0800

    version 0.1

---
+++ precache-manifest.3c1d7ef119de375af632336cbc4c2e79.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f27c9f7c681949dfc1c629c31d91b4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93e21af2093d1403958d",
+    "url": "/static/css/2.28f218dc.chunk.css"
+  },
+  {
+    "revision": "69aa60c5681dab1350b6",
+    "url": "/static/css/main.99c12ff7.chunk.css"
+  },
+  {
+    "revision": "93e21af2093d1403958d",
+    "url": "/static/js/2.50b81f04.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.50b81f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69aa60c5681dab1350b6",
+    "url": "/static/js/main.5d1820f8.chunk.js"
+  },
+  {
+    "revision": "1209add7631694ddf8b1",
+    "url": "/static/js/runtime-main.3dc933a3.js"
+  },
+  {
+    "revision": "b2d2099be870930b037073a6565c9f75",
+    "url": "/static/media/avatar.b2d2099b.jpg"
+  }
+]);
